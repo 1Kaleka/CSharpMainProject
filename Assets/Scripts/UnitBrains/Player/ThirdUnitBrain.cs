65f8a251eb5a31fb5ecb0aa60bd152043521be18
1Kaleka <threekalekiproduction@mail.ru>
2024-08-22 17:40:01 +0300

    Coment Third

--- Assets/Scripts/UnitBrains/Player/ThirdUnitBrain.cs
+++ Assets/Scripts/UnitBrains/Player/ThirdUnitBrain.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using Model.Runtime.Projectiles;
 using UnitBrains.Player;
 using UnityEngine;
@@ -9,27 +10,47 @@ public class ThirdUnitBrain : DefaultPlayerUnitBrain
 {
     public override string TargetUnitName => "Ironclad Behemoth";
 
-    private const float _timeMax = 1f;
+    private const float _maxTime = 1f;
     private bool Attack = false;
     private bool Move = false;
     private bool hasTargets = false;
     private float _startTime = 0f;
 
+    public override Vector2Int GetNextStep()
+    {
+        if (Attack || Move)
+            return unit.Pos;
+
+        return base.GetNextStep();
+    }
+
+    protected override List<Vector2Int> SelectTargets()
+    {
+        var result = base.SelectTargets();
+        hasTargets = result.Count > 0;
+        if (!Attack || Move)
+            result.Clear();
+
+        return result;
+    }
+
     public override void Update(float deltaTime, float time)
     {
         base.Update(deltaTime, time);
 
-        if (Attack && !hasTargets && !Move || !Attack && hasTargets && !Move)
+        if (Attack && !hasTargets && !Move ||
+            !Attack && hasTargets && !Move)
         {
             Move = true;
             _startTime = time;
         }
 
-        if (Move && time - _startTime >= _timeMax)
+        if (Move && time - _startTime >= _maxTime)
         {
             Attack = !Attack;
             Move = false;
         }
-    }    
+    }
 }
+
 
